610bf6966feb9cd971208a7808182a4b9efcaeca
Santyru <santiago0988@hotmail.com>
2022-09-29 18:33:01 -0300

    Create the folder for the game

---
+++ rock_paper_scissors/rock_paper_scissors.js
@@ -0,0 +1,14 @@
+/* Rock paper scissors instructions
+to play the user needs to pick rock paper or scissors
+then the computer picks one of the same three options
+rock beats scissors
+scissors cut paper
+paper wraps rock
+give the round to the winner
+if the player gets to 3 points before the computer they win*/
+
+// a container with the three choices will need to be made for the computer to choice
+
+// Print game name, welcome
+console.log("Welcom")
+// ask user for input, store in a variable called user- choice
