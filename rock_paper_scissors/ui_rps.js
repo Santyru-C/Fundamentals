a62adac7d80b0d6e263100ea38c17f34d52e355a
Santiago Cremon <santiago0988@hotmail.com>
2022-10-18 21:52:09 -0300

    Function cleanup, Add toggle function for main buttons

--- rock_paper_scissors/ui_rps.js
+++ rock_paper_scissors/ui_rps.js
@@ -1,10 +1,18 @@
 //antes que nada deberia fijarme como hacer que los botones duevuelvan un string que yo quiera
-const rockBtn = document.querySelector("#rock");
-const scissorsBtn = document.querySelector("#scissors");
-const paperBtn = document.querySelector("#paper")
+const rockBtn = document.querySelector(".handBtn.rock");
+const scissorsBtn = document.querySelector(".handBtn.scissors");
+const paperBtn = document.querySelector(".handBtn.paper")
+
+const pSelectionDisplay = document.querySelector(".player.hand")
+const cpuSelectionDisplay = document.querySelector(".cpu.hand")
+
+const roundText = document.querySelector("#roundText");
 
 const playerScore = document.querySelector(".player.score")
 const cpuScore = document.querySelector(".cpu.score")
+
+const resetButton = document.querySelector("#resetbtn")
+resetButton.addEventListener("click", function() {reset()})
 
 let cpuPoints = 0
 let playerPoints = 0
@@ -14,8 +22,7 @@ rockBtn.addEventListener("click", function() {startRound("Rock")});
 scissorsBtn.addEventListener("click", function() {startRound("Scissors")});
 paperBtn.addEventListener("click", function() {startRound("Paper")});
 
-const roundText = document.querySelector("#roundText");
-roundText.textContent = "hello"
+// functions
 
 function startRound(playerHand) {
     const cpuHand = cpuPick();
@@ -26,9 +33,13 @@ function startRound(playerHand) {
 
     if (playerPoints == 5 || cpuPoints == 5) {
         playerPoints > cpuPoints? declareMatchWinner("player"): declareMatchWinner("cpu");
+        toggleHandButtons()
     };
 
     round += 1;
+
+    pSelectionDisplay.textContent = playerHand
+    cpuSelectionDisplay.textContent = cpuHand
     updateScoreDisplay
 
     console.log(roundWinner, cpuPoints, playerPoints, cpuHand)
@@ -40,14 +51,17 @@ function selectRoundWinner(player, cpu) {
         case "RockScissors":
         case "ScissorsPaper":
         case "PaperRock":
+            roundText.textContent = "You win! " + player + " beats " + cpu; //misc update, move this functionality to a proper function
             return "Player";
 
         case "RockRock":
         case "ScissorsScissors":
         case "PaperPaper":
+            roundText.textContent = "Draw!";
             return "Draw";
 
         default:
+            roundText.textContent = "You lose! " + cpu + " beats " + player;
             return "Cpu";  
     };
 };
@@ -67,11 +81,6 @@ function addPoint(roundWinner) {
     };
 };
 
-function resetCounter() {
-    cpuPoints = 0
-    playerPoints = 0
-}
-
 function cpuPick() {
 
     const rps = ["Rock", "Paper", "Scissors"];
@@ -81,20 +90,12 @@ function cpuPick() {
     return consoleSelection
 };
 
-function resetScore() {
-    cpuPoints = 0;
-    playerPoints = 0;
-};
-
 function declareMatchWinner(winner) {
-    winner == "player"? console.log("You win"): console.log("You lose");
+    winner == "player"? roundText.textContent = "Game is over: You win!": 
+                        roundText.textContent = "Game is over: You lose!";
 
-    playerPoints = 0;
-    cpuPoints = 0;
-    reset()
 };
 
-// una funcion que actualice el marcador de los jugadores
 function updateScoreDisplay() {
     playerScore.textContent = playerPoints;
     cpuScore.textContent = cpuPoints;
@@ -104,8 +105,22 @@ function reset() {
     playerPoints = 0
     cpuPoints = 0
     round = 0
-    cpuScore.textContent = "0"
-    playerScore.textContent = "0"
+
+    cpuScore.textContent = playerPoints
+    playerScore.textContent = cpuPoints
+    roundText.textContent = "Rock... Paper... Scissors!"
+    toggleHandButtons()
+
+};
+
+function toggleHandButtons() {
+    const allHandButtons = document.querySelectorAll(".handBtn");
+
+    for (const button of allHandButtons) {
+        button.disabled? button.disabled = false: button.disabled = true;
+    };
+
 }
+
 
 // play again button
